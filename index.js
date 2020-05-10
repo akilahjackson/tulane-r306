69d49395b5717c0b4fd185d04e95d4511ba4c1c4
akilahjackson <akilah@mindmapengineers.com>
2020-05-10 14:54:24 -0500

    adding the index.js controller to PWA

---
+++ index.js
@@ -0,0 +1,11 @@
+// Start the TU Experience 
+
+import { tu_experience } from './settings.js';
+
+tu_experience ('start');
+
+// Establish Communications
+
+import { tu_ws1 } from './messaging.js';
+
+tu_ws1('ATC').onOpen = " This world needs and enema";
